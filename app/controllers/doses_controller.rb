a09e22d2f7868096f27d47a9fd779e2fb0222076
David Garcia <davidmd.garcia@gmail.com>
2019-11-07 23:47:28 +0900

    mister cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,40 @@
+class DosesController < ApplicationController
+  before_action :find_cocktail, only: [:new, :create]
+
+
+  def new
+    @dose = Dose.new
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def index
+    @doses = Dose.all
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
